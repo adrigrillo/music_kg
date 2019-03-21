bb9b4a64a1f39d72909ead79827a9a24f3b3d29d
Adrián Rodríguez Grillo <adrirgrillo@gmail.com>
2019-03-21 17:16:04 +0100

    Alias removed moved to a new triple set

--- sql/query-countries.sql
+++ sql/query-countries.sql
@@ -1,17 +1,12 @@
--- This query add the alias into a list
-SELECT gid, area.name AS name, with_alias.aliases AS aliases, code
+-- This query links all the alias to the areas
+SELECT area.gid AS gid, area_alias.name AS alias
 FROM area
-       INNER JOIN iso_3166_1 AS iso
-                  ON area.id = iso.area
-       LEFT JOIN (SELECT a.name, string_agg(area_alias.name, ', ') AS aliases
-                  FROM area a
-                         INNER JOIN area_alias
-                                    ON a.id = area_alias.area
-                  GROUP BY a.name) AS with_alias
-                 ON area.name = with_alias.name;
+       LEFT JOIN area_alias
+                 ON area.id = area_alias.area
+WHERE area.name != area_alias.name;
 
 -- Without considering the alias
-SELECT gid, id, name, code
+SELECT gid, name, code
 FROM area
        INNER JOIN iso_3166_1
                   ON area.id = iso_3166_1.area;
--- sql/query-other_areas.sql
+++ sql/query-other_areas.sql
@@ -1,13 +1,6 @@
 -- Select Subdivision
-SELECT area.gid, area.name AS name, with_alias.aliases AS aliases, relation.gid AS parent_gid
+SELECT area.gid, area.name AS name, relation.gid AS parent_gid
 FROM area
-       -- With this we obtain the aliases of the area
-       LEFT JOIN (SELECT a.name, string_agg(area_alias.name, ', ') AS aliases
-                  FROM area a
-                         INNER JOIN area_alias
-                                    ON a.id = area_alias.area
-                  GROUP BY a.name) AS with_alias
-                 ON area.name = with_alias.name
        -- With this we obtain the gid and the name of the parent area
        JOIN (SELECT entity0, entity1, link, area_parent.gid
              FROM l_area_area rel_area
@@ -17,18 +10,12 @@ FROM area
        JOIN (SELECT id, link_type
              FROM link) AS lk_tp
             ON relation.link = lk_tp.id
-WHERE type = 2 AND lk_tp.link_type = 356;
+WHERE type = 2
+  AND lk_tp.link_type = 356;
 
 -- Select cities
-SELECT area.gid, area.name AS name, with_alias.aliases AS aliases, relation.gid AS parent_gid
+SELECT area.gid, area.name AS name, relation.gid AS parent_gid
 FROM area
-       -- With this we obtain the aliases of the area
-       LEFT JOIN (SELECT a.name, string_agg(area_alias.name, ', ') AS aliases
-                  FROM area a
-                         INNER JOIN area_alias
-                                    ON a.id = area_alias.area
-                  GROUP BY a.name) AS with_alias
-                 ON area.name = with_alias.name
        -- With this we obtain the gid and the name of the parent area
        JOIN (SELECT entity0, entity1, link, area_parent.gid
              FROM l_area_area rel_area
@@ -38,18 +25,12 @@ FROM area
        JOIN (SELECT id, link_type
              FROM link) AS lk_tp
             ON relation.link = lk_tp.id
-WHERE type = 3 AND lk_tp.link_type = 356;
+WHERE type = 3
+  AND lk_tp.link_type = 356;
 
 -- Select municipalities
-SELECT area.gid, area.name AS name, with_alias.aliases AS aliases, relation.gid AS parent_gid
+SELECT area.gid, area.name AS name, relation.gid AS parent_gid
 FROM area
-       -- With this we obtain the aliases of the area
-       LEFT JOIN (SELECT a.name, string_agg(area_alias.name, ', ') AS aliases
-                  FROM area a
-                         INNER JOIN area_alias
-                                    ON a.id = area_alias.area
-                  GROUP BY a.name) AS with_alias
-                 ON area.name = with_alias.name
        -- With this we obtain the gid and the name of the parent area
        JOIN (SELECT entity0, entity1, link, area_parent.gid
              FROM l_area_area rel_area
@@ -59,18 +40,12 @@ FROM area
        JOIN (SELECT id, link_type
              FROM link) AS lk_tp
             ON relation.link = lk_tp.id
-WHERE type = 4 AND lk_tp.link_type = 356;
+WHERE type = 4
+  AND lk_tp.link_type = 356;
 
 -- Select district
-SELECT area.gid, area.name AS name, with_alias.aliases AS aliases, relation.gid AS parent_gid
+SELECT area.gid, area.name AS name, relation.gid AS parent_gid
 FROM area
-       -- With this we obtain the aliases of the area
-       LEFT JOIN (SELECT a.name, string_agg(area_alias.name, ', ') AS aliases
-                  FROM area a
-                         INNER JOIN area_alias
-                                    ON a.id = area_alias.area
-                  GROUP BY a.name) AS with_alias
-                 ON area.name = with_alias.name
        -- With this we obtain the gid and the name of the parent area
        JOIN (SELECT entity0, entity1, link, area_parent.gid
              FROM l_area_area rel_area
@@ -80,18 +55,12 @@ FROM area
        JOIN (SELECT id, link_type
              FROM link) AS lk_tp
             ON relation.link = lk_tp.id
-WHERE type = 5 AND lk_tp.link_type = 356;
+WHERE type = 5
+  AND lk_tp.link_type = 356;
 
 -- Select island
-SELECT area.gid, area.name AS name, with_alias.aliases AS aliases, relation.gid AS parent_gid
+SELECT area.gid, area.name AS name, relation.gid AS parent_gid
 FROM area
-       -- With this we obtain the aliases of the area
-       LEFT JOIN (SELECT a.name, string_agg(area_alias.name, ', ') AS aliases
-                  FROM area a
-                         INNER JOIN area_alias
-                                    ON a.id = area_alias.area
-                  GROUP BY a.name) AS with_alias
-                 ON area.name = with_alias.name
        -- With this we obtain the gid and the name of the parent area
        JOIN (SELECT entity0, entity1, link, area_parent.gid
              FROM l_area_area rel_area
@@ -101,18 +70,12 @@ FROM area
        JOIN (SELECT id, link_type
              FROM link) AS lk_tp
             ON relation.link = lk_tp.id
-WHERE type = 6 AND lk_tp.link_type = 356;
+WHERE type = 6
+  AND lk_tp.link_type = 356;
 
 -- Select county
-SELECT area.gid, area.name AS name, with_alias.aliases AS aliases, relation.gid AS parent_gid
+SELECT area.gid, area.name AS name, relation.gid AS parent_gid
 FROM area
-       -- With this we obtain the aliases of the area
-       LEFT JOIN (SELECT a.name, string_agg(area_alias.name, ', ') AS aliases
-                  FROM area a
-                         INNER JOIN area_alias
-                                    ON a.id = area_alias.area
-                  GROUP BY a.name) AS with_alias
-                 ON area.name = with_alias.name
        -- With this we obtain the gid and the name of the parent area
        JOIN (SELECT entity0, entity1, link, area_parent.gid
              FROM l_area_area rel_area
@@ -122,4 +85,5 @@ FROM area
        JOIN (SELECT id, link_type
              FROM link) AS lk_tp
             ON relation.link = lk_tp.id
-WHERE type = 7 AND lk_tp.link_type = 356;
+WHERE type = 7
+  AND lk_tp.link_type = 356;
