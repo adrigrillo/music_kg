3b251d7c28487ab630f3de28db06e8b399e0e8fe
Adrián Rodríguez Grillo <adrirgrillo@gmail.com>
2019-03-23 13:14:38 +0100

    Merged area queries

--- sql/query-areas.sql
+++ sql/query-areas.sql
@@ -1,3 +1,9 @@
+-- Select countries
+SELECT gid, name, code
+FROM area
+       INNER JOIN iso_3166_1
+                  ON area.id = iso_3166_1.area;
+
 -- Select Subdivision
 SELECT area.gid, area.name AS name, relation.gid AS parent_gid
 FROM area
@@ -87,3 +93,10 @@ FROM area
             ON relation.link = lk_tp.id
 WHERE type = 7
   AND lk_tp.link_type = 356;
+
+-- This query links all the alias to the areas
+SELECT area.gid AS gid, area_alias.name AS alias
+FROM area
+       LEFT JOIN area_alias
+                 ON area.id = area_alias.area
+WHERE area.name != area_alias.name;
