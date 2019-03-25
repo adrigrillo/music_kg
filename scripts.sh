9bf6a2b9699714ee05da6639c531e1c3c55be378
Adrián Rodríguez Grillo <adrirgrillo@gmail.com>
2019-03-25 16:57:21 +0100

    Included first configuration file LIMES

--- scripts.sh
+++ scripts.sh
@@ -3,3 +3,6 @@ docker run -it --rm --network="host" -v ~/Documents/knowledge_graphs/project/gra
 
 # Command to generate the genre graph
 java -jar ~/Apps/rmlmapper.jar -m ~/Documents/knowledge_graphs/project/graph/genres-mapping.ttl -o ~/Documents/knowledge_graphs/project/graph/output/genres-graph-part.nt -s nquads -v && gzip -f -v ~/Documents/knowledge_graphs/project/graph/output/genres-graph-part.nt
+
+# Running limes
+cd ~/Documents/knowledge_graphs/project/graph/limes && java -jar ~/Apps/LIMES/limes-core-1.5.6.jar ./groups_dbpedia-config.xml
